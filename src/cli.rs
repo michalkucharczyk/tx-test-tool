c4ce85726d509d1cd51dcf12b4ddcf43a6b3a6d0
Iulian Barbu <iulian.barbu@parity.io>
2025-02-11 21:43:42 +0000

    redo some changes
    Signed-off-by: Iulian Barbu <iulian.barbu@parity.io>

--- src/cli.rs
+++ src/cli.rs
@@ -9,6 +9,106 @@ pub struct Cli {
 	pub command: CliCommand,
 }
 
+#[derive(Subcommand)]
+pub enum CliCommand {
+	Tx {
+		/// The type of chain to be used.
+		#[clap(long, default_value = "sub")]
+		chain: ChainType,
+		/// The RPC endpoint of the node to be used.
+		#[clap(long, default_value = "ws://127.0.0.1:9933")]
+		ws: String,
+		/// Send transaction with event listener (submit_and_watch).
+		#[clap(long)]
+		unwatched: bool,
+		/// Spawn block monitor for checking if transactions are included in finalized blocks.
+		#[clap(long)]
+		block_monitor: bool,
+		/// Use mortal transactions.
+		#[clap(long)]
+		mortal: Option<u32>,
+		/// Send transactions threshold, sends the batch when number of pedning extrinsics drops
+		/// below this number.
+		#[clap(long, default_value_t = 10000)]
+		send_threshold: u32,
+		/// Override log file name (out_yyyymmdd_hhmmss.json)
+		#[clap(long)]
+		log_file: Option<String>,
+		/// Use remark command with given size in kbytes. If not given transfer transaction will be
+		/// sent.
+		#[clap(long)]
+		remark: Option<u32>,
+		/// Accounts range used for building/seding transactions.
+		#[clap(subcommand)]
+		scenario: SendingScenario,
+	},
+	/// Check nonce for given account.
+	CheckNonce {
+		/// The type of chain to be used.
+		#[clap(long, default_value = "sub")]
+		chain: ChainType,
+		/// The RPC endpoint of the node to be used.
+		#[clap(long, default_value = "ws://127.0.0.1:9933")]
+		ws: String,
+		/// Account identifier to be used. It can be keyring account (alice, bob,...) or index of
+		/// pre-funded account index used for derivation.
+		#[clap(long)]
+		account: String,
+	},
+	/// Download and display the metadata.
+	Metadata {
+		/// The RPC endpoint of the node to be used.
+		#[clap(long, default_value = "ws://127.0.0.1:9933")]
+		ws: String,
+	},
+	/// Execute the stand alone block monitor and print some transactions stats.
+	BlockMonitor {
+		/// The type of chain to be used.
+		#[clap(long, default_value = "sub")]
+		chain: ChainType,
+		/// The RPC endpoint of the node to be used.
+		#[clap(long, default_value = "ws://127.0.0.1:9933")]
+		ws: String,
+	},
+	/// Load and inspect existing log file.
+	LoadLog {
+		/// The type of chain used to store the log file..
+		#[clap(long, default_value = "sub")]
+		chain: ChainType,
+		/// Name of the file to be loaded.
+		log_file: String,
+		#[clap(long)]
+		/// Display some histograms.
+		show_graphs: bool,
+		/// Display errors.
+		#[clap(long)]
+		show_errors: bool,
+	},
+	/// Generate a list of endowed accounts.
+	GenerateEndowedAccounts {
+		/// The type of chain used to store the log file..
+		#[clap(long, default_value = "sub")]
+		chain: ChainType,
+		/// First account identifier to be generated (index of the account used for a
+		/// derivation).
+		#[clap(long)]
+		start_id: u32,
+		/// Last account identifier to be generated.
+		#[clap(long)]
+		last_id: u32,
+		/// Initial balance
+		#[clap(long, default_value = "100000000000000")]
+		balance: u128,
+		/// File where patch with funded accounts json will be stored. Or if the input chain-spec
+		/// was provided, the location of chain spec with endowed accounts injected.
+		#[clap(long)]
+		out_file_name: String,
+		/// The plain chain spec file that will be used to inject endowed accounts into.
+		#[clap(long)]
+		chain_spec: Option<String>,
+	},
+}
+
 #[derive(ValueEnum, Clone)]
 pub enum ChainType {
 	/// Substrate compatible chain.
@@ -84,104 +184,4 @@ impl SendingScenario {
 				},
 		}
 	}
-}
-
-#[derive(Subcommand)]
-pub enum CliCommand {
-	Tx {
-		/// The type of chain to be used.
-		#[clap(long, default_value = "sub")]
-		chain: ChainType,
-		/// The RPC endpoint of the node to be used.
-		#[clap(long, default_value = "ws://127.0.0.1:9933")]
-		ws: String,
-		/// Send transaction with event listener (submit_and_watch).
-		#[clap(long)]
-		unwatched: bool,
-		/// Spawn block monitor for checking if transactions are included in finalized blocks.
-		#[clap(long)]
-		block_monitor: bool,
-		/// Use mortal transactions.
-		#[clap(long)]
-		mortal: Option<u32>,
-		/// Send transactions threshold, sends the batch when number of pedning extrinsics drops
-		/// below this number.
-		#[clap(long, default_value_t = 10000)]
-		send_threshold: u32,
-		/// Override log file name (out_yyyymmdd_hhmmss.json)
-		#[clap(long)]
-		log_file: Option<String>,
-		/// Use remark command with given size in kbytes. If not given transfer transaction will be
-		/// sent.
-		#[clap(long)]
-		remark: Option<u32>,
-		/// Type of scenario - transfer or remark.
-		#[clap(subcommand)]
-		scenario: SendingScenario,
-	},
-	/// Check nonce for given account.
-	CheckNonce {
-		/// The type of chain to be used.
-		#[clap(long, default_value = "sub")]
-		chain: ChainType,
-		/// The RPC endpoint of the node to be used.
-		#[clap(long, default_value = "ws://127.0.0.1:9933")]
-		ws: String,
-		/// Account identifier to be used. It can be keyring account (alice, bob,...) or index of
-		/// pre-funded account index used for derivation.
-		#[clap(long)]
-		account: String,
-	},
-	/// Download and display the metadata.
-	Metadata {
-		/// The RPC endpoint of the node to be used.
-		#[clap(long, default_value = "ws://127.0.0.1:9933")]
-		ws: String,
-	},
-	/// Execute the stand alone block monitor and print some transactions stats.
-	BlockMonitor {
-		/// The type of chain to be used.
-		#[clap(long, default_value = "sub")]
-		chain: ChainType,
-		/// The RPC endpoint of the node to be used.
-		#[clap(long, default_value = "ws://127.0.0.1:9933")]
-		ws: String,
-	},
-	/// Load and inspect existing log file.
-	LoadLog {
-		/// The type of chain used to store the log file..
-		#[clap(long, default_value = "sub")]
-		chain: ChainType,
-		/// Name of the file to be loaded.
-		log_file: String,
-		#[clap(long)]
-		/// Display some histograms.
-		show_graphs: bool,
-		/// Display errors.
-		#[clap(long)]
-		show_errors: bool,
-	},
-	/// Generate a list of endowed accounts.
-	GenerateEndowedAccounts {
-		/// The type of chain used to store the log file..
-		#[clap(long, default_value = "sub")]
-		chain: ChainType,
-		/// First account identifier to be generated (index of the account used for a
-		/// derivation).
-		#[clap(long)]
-		start_id: u32,
-		/// Last account identifier to be generated.
-		#[clap(long)]
-		last_id: u32,
-		/// Initial balance
-		#[clap(long, default_value = "100000000000000")]
-		balance: u128,
-		/// File where patch with funded accounts json will be stored. Or if the input chain-spec
-		/// was provided, the location of chain spec with endowed accounts injected.
-		#[clap(long)]
-		out_file_name: String,
-		/// The plain chain spec file that will be used to inject endowed accounts into.
-		#[clap(long)]
-		chain_spec: Option<String>,
-	},
 }
