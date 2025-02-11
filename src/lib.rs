b69aa3c56053745701aacddf6edf3a8192767df0
Iulian Barbu <iulian.barbu@parity.io>
2025-02-11 23:04:18 +0000

    added crate-level docs
    Signed-off-by: Iulian Barbu <iulian.barbu@parity.io>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,48 @@
+//! This is an experimental library that can be used to send transactions
+//! to a substrate network and monitor them in terms of how many have been
+//! submitted to the transaction pool, validated, broadcasted, inlcluded in blocks,
+//! finalized, invalid or dropped.
+//!
+//! There is an associated binary that can be used as a CLI, an alternative to using
+//! the library.
+//!
+//! Example:
+//!  ```rust,ignore
+//!  fn test() {
+//!	    // Shared params.
+//!     let send_threshold = 20_000;
+//!     let ws = "ws://127.0.0.1:9933";
+//!     let recipe_future = TransactionRecipe::transfer();
+//!     let recipe_ready = recipe_future.clone();
+//!     let block_monitor = false;
+//!     let unwatched = false;
+//!
+//!     // Scenarios and sinks.
+//!     let scenario_future =
+//!       SendingScenario::FromManyAccounts { start_id: 0, last_id: 99, from: Some(100), count: 100
+//! };     let scenario_ready =
+//!       SendingScenario::FromManyAccounts { start_id: 0, last_id: 99, from: Some(0), count: 100 };
+//
+//!     let future_scenario_executor =
+//!	      ScenarioExecutor::new(ws, scenario_future, recipe_future, block_monitor).await;
+//!     let ready_scenario_executor =
+//!	      ScenarioExecutor::new(ws, scenario_ready, recipe_ready, block_monitor).await;
+//!
+//!     let ((future_stop_runner_tx, mut runner_future), future_queue_task) =
+//!	      RunnerFactory::substrate_runner(future_scenario_executor, send_threshold,
+//! unwatched).await; 	    let ((ready_stop_runner_tx, mut runner_ready), ready_queue_task) =
+//!	      RunnerFactory::substrate_runner(ready_scenario_executor, send_threshold, unwatched).await;
+//!
+//!     let (future_logs, ready_logs) = join(runner_future.run_poc2(),
+//! runner_ready.run_poc2()).await;     let finalized_future =
+//!	      future_logs.values().filter_map(|default_log| default_log.finalized()).count();
+//!     let finalized_ready =
+//!	      ready_logs.values().filter_map(|default_log| default_log.finalized()).count();
+//!     assert_eq!(finalized_future, 100);
+//!     assert_eq!(finalized_ready, 100);
+//!  }
+//!  ```
+
 use crate::transaction::Transaction;
 
 use crate::{
