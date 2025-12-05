8cbd808f85ccb06b4d24f5400cdd784f5e8e300c
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2025-12-05 09:11:48 +0100

    again

--- examples/kill_dev_entry.rs
+++ examples/kill_dev_entry.rs
@@ -4,9 +4,8 @@
 
 //! Example: Send `TestPallet::kill_dev_entry` transactions using a custom payload builder.
 //!
-//! This example demonstrates how to use `with_tx_payload_builder_sub` build a custom spammer tool
-//! using custom transactions. Each account sends one transaction that deletes 5 entries starting at
-//! index `5 * account_id`.
+//! This example demonstrates how to use `with_tx_payload_builder_sub` to build a custom spammer
+//! tool using specific transactions.
 //!
 //! Usage:
 //! ```bash
